6eb7c78c72910864a99f8066925414d2ea958183
Josh Buker <apate+git@nyx.chat>
2018-12-29 02:27:35 -0800

    Add Basic Classification README & initial file

---
+++ basic-classification/README.md
@@ -0,0 +1,4 @@
+# Basic Classification
+
+Per TensorFlow getting started docs: [Basic Classification](https://www.tensorflow.org/tutorials/keras/basic_classification)
+
---
+++ basic-classification/basic_classification.py
@@ -0,0 +1,10 @@
+# TensorFlow & Keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Numpy & Matplot
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
