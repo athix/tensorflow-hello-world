fb0c8167bcd01d350de02096309490a24676d274
Josh Buker <apate+git@nyx.chat>
2018-12-29 02:50:09 -0800

    Add basic preprocessing per tutorial (neat)

--- basic-classification/basic_classification.py
+++ basic-classification/basic_classification.py
@@ -13,3 +13,39 @@ print(tf.__version__)
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+# Classifications
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Interesting print statements
+print(train_images.shape) # (60000, 28, 28)
+print(len(train_labels))  # 60000
+print(train_labels)       # array([9, 0, 0, ..., 3, 0, 5], dtype=uint8)
+print(test_images.shape)  # (10000, 28, 28)
+print(len(test_labels))   # 10000
+
+# Preprocess data
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+
+# View plot
+# plt.show()
+
+# Consistent preprocessing
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+
+# View plot
+# plt.show()
+
