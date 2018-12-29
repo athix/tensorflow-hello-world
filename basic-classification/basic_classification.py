cc4d3d72e2d79b40ab3991ebcff665de8d0ae287
Josh Buker <apate+git@nyx.chat>
2018-12-29 02:34:38 -0800

    Add fashion datasets

--- basic-classification/basic_classification.py
+++ basic-classification/basic_classification.py
@@ -6,5 +6,10 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Version >= 1.12.0
 print(tf.__version__)
+
+# Download fashion dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
