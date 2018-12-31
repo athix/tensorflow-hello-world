17bca843f35687953ec00a93c583b5cc0f3e1fb8
Josh Buker <apate+git@nyx.chat>
2018-12-31 01:31:22 -0800

    Add predictions and fancy ankle boot graphic

--- basic-classification/basic_classification.py
+++ basic-classification/basic_classification.py
@@ -69,3 +69,54 @@ test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 print('Test accuracy:', test_acc)
 
+# Make predictions
+predictions = model.predict(test_images)
+
+print(predictions[0]) # array of 10 numbers representing the confidence in each label
+
+print(np.argmax(predictions[0])) # 9 -> most confident in label 9 (ankle boot), which we can check...
+print(test_labels[0]) # also 9...
+
+## Fancy prediction graphs
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.imshow(img, cmap=plt.cm.binary)
+
+    predicted_label = np.argmax(predictions_array)
+    if predicted_label == true_label:
+        color = 'blue'
+    else:
+        color = 'red'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+        100*np.max(predictions_array),
+        class_names[true_label]),
+        color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+    predictions_array, true_label = predictions_array[i], true_label[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    thisplot = plt.bar(range(10), predictions_array, color="#777777")
+    plt.ylim([0, 1])
+    predicted_label = np.argmax(predictions_array)
+
+    thisplot[predicted_label].set_color('red')
+    thisplot[true_label].set_color('blue')
+
+## The famous Ankle boot
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions, test_labels, test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions, test_labels)
+
+# View our fancy Ankle boot graphic
+plt.show()
+
