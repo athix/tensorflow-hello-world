d58a40dc91484199fb6537df405764c058fff006
Josh Buker <apate+git@nyx.chat>
2018-12-31 01:51:01 -0800

    Add fancy graph with 12 predictions

--- basic-classification/basic_classification.py
+++ basic-classification/basic_classification.py
@@ -129,5 +129,19 @@ plt.subplot(1,2,2)
 plot_value_array(i, predictions, test_labels)
 
 # View our terrible prediction
+# plt.show()
+
+## Once more, with feeling
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+    plt.subplot(num_rows, 2*num_cols, 2*i+1)
+    plot_image(i, predictions, test_labels, test_images)
+    plt.subplot(num_rows, 2*num_cols, 2*i+2)
+    plot_value_array(i, predictions, test_labels)
+
+# View best graphic yet
 plt.show()
 
