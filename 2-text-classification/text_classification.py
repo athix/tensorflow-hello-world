02814a8fde47a56049d1719f31fab93b92f355ec
Josh Buker <apate+git@nyx.chat>
2019-01-03 15:04:06 -0800

    Add training accuracy graph

--- 2-text-classification/text_classification.py
+++ 2-text-classification/text_classification.py
@@ -141,4 +141,14 @@ plt.ylabel('Loss')
 plt.legend()
 
 plt.show()
+plt.clf() # clear figure
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.show()
 
