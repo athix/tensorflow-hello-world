367700bac941214040be5b28f6ca76178db91050
Josh Buker <apate+git@nyx.chat>
2019-01-09 15:57:18 -0800

    Download dataset and display multi-hot vector

--- 4-overfit-and-underfit/overfit_and_underfit.py
+++ 4-overfit-and-underfit/overfit_and_underfit.py
@@ -10,3 +10,25 @@ import matplotlib.pyplot as plt
 
 print(tf.__version__) # >= 1.12.0
 
+######################
+## Download Dataset ##
+######################
+
+NUM_WORDS = 10000
+
+(train_data, train_labels), (test_data, test_labels) = keras.datasets.imdb.load_data(num_words=NUM_WORDS)
+
+def multi_hot_sequences(sequences, dimension):
+    # Create an all-zero matrix of shape (len(sequences), dimension)
+    results = np.zeros((len(sequences), dimension)) # Why does this double enclose? Python quirk?
+    for i, word_indices in enumerate(sequences):
+        results[i, word_indices] = 1.0 # set specific indices of results[i] to 1s
+    return results
+
+train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
+test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
+
+# View multi-hot vectors
+plt.plot(train_data[0])
+plt.show()
+
