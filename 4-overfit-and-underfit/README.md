f1dfbc06155ec7731a49b72b18a874982726c56f
Josh Buker <apate+git@nyx.chat>
2019-01-09 15:39:25 -0800

    Add overfit and underfit tutorial

---
+++ 4-overfit-and-underfit/README.md
@@ -0,0 +1,4 @@
+# Explore overfitting and underfitting
+
+Per TensorFlow getting started docs: [Overfit and Underfit](https://www.tensorflow.org/tutorials/keras/overfit_and_underfit)
+
---
+++ 4-overfit-and-underfit/overfit_and_underfit.py
@@ -0,0 +1,12 @@
+###########
+## Setup ##
+###########
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__) # >= 1.12.0
+
