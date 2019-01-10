f2d3f3a27cc9c48e286dce0fb5a8017131f4ee13
Josh Buker <apate+git@nyx.chat>
2019-01-10 13:23:08 -0800

    Add final tutorial, save and restore models

---
+++ 5-save-and-restore-models/README.md
@@ -0,0 +1,4 @@
+# Save and restore models
+
+Per TensorFlow getting started docs: [Save and restore models](https://www.tensorflow.org/tutorials/keras/save_and_restore_models)
+
---
+++ 5-save-and-restore-models/save_and_restore.py
@@ -0,0 +1,13 @@
+###########
+## Setup ##
+###########
+
+from __future__ import absolute_import, division, print_function
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.__version__) # >= 1.12.0
+
