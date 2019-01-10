854f28bed6ded88476cade830cba8208300b4735
Josh Buker <apate+git@nyx.chat>
2019-01-09 19:48:04 -0800

    Add smaller model

--- 4-overfit-and-underfit/overfit_and_underfit.py
+++ 4-overfit-and-underfit/overfit_and_underfit.py
@@ -29,8 +29,8 @@ train_data = multi_hot_sequences(train_data, dimension=NUM_WORDS)
 test_data = multi_hot_sequences(test_data, dimension=NUM_WORDS)
 
 # View multi-hot vectors
-plt.plot(train_data[0])
-plt.show()
+#plt.plot(train_data[0])
+#plt.show()
 
 #############################
 ## Demonstrate Overfitting ##
@@ -53,6 +53,29 @@ baseline_model.compile(
 print(baseline_model.summary())
 
 baseline_history = baseline_model.fit(
+        train_data,
+        train_labels,
+        epochs=20,
+        batch_size=512,
+        validation_data=(test_data, test_labels),
+        verbose=2)
+
+# Create a smaller model
+
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(4, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+smaller_model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy', 'binary_crossentropy'])
+
+print(smaller_model.summary())
+
+smaller_history = smaller_model.fit(
         train_data,
         train_labels,
         epochs=20,
