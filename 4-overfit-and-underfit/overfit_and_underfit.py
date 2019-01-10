4757a0771ae484a4c1f6040979668d6ba8505ef3
Josh Buker <apate+git@nyx.chat>
2019-01-09 20:22:13 -0800

    Add graph of training history

--- 4-overfit-and-underfit/overfit_and_underfit.py
+++ 4-overfit-and-underfit/overfit_and_underfit.py
@@ -107,3 +107,34 @@ bigger_history = bigger_model.fit(
         validation_data=(test_data, test_labels),
         verbose=2)
 
+#######################################
+## Plot training and validation loss ##
+#######################################
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16,10))
+
+    for name, history in histories:
+        val = plt.plot(
+                history.epoch,
+                history.history['val_'+key],
+                '--',
+                label=name.title()+' Val')
+        plt.plot(
+                history.epoch,
+                history.history[key],
+                color=val[0].get_color(),
+                label=name.title()+' Train')
+
+    plt.xlabel('Epochs')
+    plt.ylabel(key.replace('_',' ').title())
+    plt.legend()
+
+    plt.xlim([0, max(history.epoch)])
+    plt.show()
+
+plot_history([
+    ('baseline', baseline_history),
+    ('smaller', smaller_history),
+    ('bigger', bigger_history)])
+
