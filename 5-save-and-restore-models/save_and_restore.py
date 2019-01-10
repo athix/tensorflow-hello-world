2609abf960d74542bdbb4c1851c865af0f910955
Josh Buker <apate+git@nyx.chat>
2019-01-10 13:56:13 -0800

    Add model and checkpoints

--- 5-save-and-restore-models/save_and_restore.py
+++ 5-save-and-restore-models/save_and_restore.py
@@ -23,3 +23,41 @@ test_labels = test_labels[:1000]
 train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
 test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
 
+####################
+## Define a model ##
+####################
+
+def create_model():
+    model = tf.keras.models.Sequential([
+        keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(784,)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.sparse_categorical_crossentropy,
+                  metrics=['accuracy'])
+
+    return model
+
+# Create a basic model instance
+model = create_model()
+model.summary()
+
+#################
+## Checkpoints ##
+#################
+
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create checkpoint callback
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        checkpoint_path,
+        save_weights_only=True,
+        verbose=1)
+
+model.fit(train_images, train_labels, epochs=10,
+          validation_data = (test_images, test_labels),
+          callbacks = [checkpoint_callback]) # pass callback to training
+
