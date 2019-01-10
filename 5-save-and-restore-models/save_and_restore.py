075455d05224b0ac34cf2220b249a0e84af815ab
Josh Buker <apate+git@nyx.chat>
2019-01-10 13:44:27 -0800

    Download mnist dataset

--- 5-save-and-restore-models/save_and_restore.py
+++ 5-save-and-restore-models/save_and_restore.py
@@ -11,3 +11,15 @@ from tensorflow import keras
 
 print(tf.__version__) # >= 1.12.0
 
+######################
+## Download Dataset ##
+######################
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+
+train_labels = train_labels[:1000]
+test_labels = test_labels[:1000]
+
+train_images = train_images[:1000].reshape(-1, 28 * 28) / 255.0
+test_images = test_images[:1000].reshape(-1, 28 * 28) / 255.0
+
